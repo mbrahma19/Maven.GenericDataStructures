ecfab2a1e4b090f34a74911107e3b70472758add
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-22 21:38:41 -0400

    Completed ArrayListCombiner and MapFunc

--- src/main/java/ArrayListCombiner/ArrayListCombiner.java
+++ src/main/java/ArrayListCombiner/ArrayListCombiner.java
@@ -1,6 +1,7 @@
 package ArrayListCombiner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Create two generic methods that take two arraylists.  The methods should both append the second ArrayList's items,
@@ -9,4 +10,12 @@ import java.util.ArrayList;
  * The second method should be called superCombiner and should use ? super E
  */
 public class ArrayListCombiner {
+
+    public static<E> void extendCombiner(ArrayList<E> list1, ArrayList<? extends E> list2){
+        list1.addAll(list2);
+    }
+
+    public static<E> void superCombiner(List<? super E> list1, List<E> list2){
+        list1.addAll(list2);
+    }
 }
--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -1,12 +1,19 @@
 package MapFunc;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Create a function called `map` that takes an ArrayList and a `Function<T,R>` object,
  * and returns an ArrayList with all of the elements of the first after the function is applied to them.
  */
 public class MapFunc {
+
+    public static<T,R> ArrayList<R> map(ArrayList<T> list, Function<T,R> function){
+        return list.stream().map(function).collect(Collectors.toCollection(ArrayList::new));
+    }
 
 }
