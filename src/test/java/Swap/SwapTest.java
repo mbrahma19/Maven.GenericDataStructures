472d4f0049eb8f353d522d5e3afa43e5726f7766
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-23 19:10:01 -0400

    Completed correct changes

--- src/test/java/Swap/SwapTest.java
+++ src/test/java/Swap/SwapTest.java
@@ -9,8 +9,8 @@ import org.junit.Test;
 public class SwapTest {
     @Test
     public void testSwap() throws Exception {
-        Number[] result = Swap.swap(0,1, 1.5, 2, 3);
-        Number[] expected = {2.0, 1.5, 3.0};
+        Double[] result = Swap.swap(0,1, 1.5, 2.0, 3.0);
+        Double[] expected = {2.0, 1.5, 3.0};
         Assert.assertArrayEquals(expected, result);
     }
 }
